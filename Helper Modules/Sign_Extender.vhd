98cb2efd12549f32c0bf88c3c2ad7230a65afb10
ma7moud-7amdy <ma7moud.7amdy93@gmail.com>
2025-05-04 05:53:04 +0300

    Add files via upload

--- Helper Modules/Sign_Extender.vhd
+++ Helper Modules/Sign_Extender.vhd
@@ -1,39 +1,13 @@
--------------------------------------------------------------------------------
---
--- Title       : sign_extend
--- Design      : ALU
--- Author      : Dell
--- Company     : student
---
--------------------------------------------------------------------------------
---
--- File        : c:/My_Designs/hardware/ALU/src/sign_extend.vhd
--- Generated   : Mon Apr 28 17:01:15 2025
--- From        : Interface description file
--- By          : ItfToHdl ver. 1.0
---
--------------------------------------------------------------------------------
---
--- Description : 
---
--------------------------------------------------------------------------------
-
---{{ Section below this comment is automatically maintained
---    and may be overwritten
---{entity {sign_extend} architecture {convert}}
-
 library IEEE;
 use IEEE.std_logic_1164.all; 
 use IEEE.NUMERIC_STD.ALL;
 
 entity sign_extend is
 	port(
-	input : in STD_LOGIC_Vector(15 downto 0);
+	input : in STD_LOGIC_Vector(15 downto 0):="1000000011111111";
 	output : out STD_LOGIC_Vector(31 downto 0)
 	);
 end sign_extend;
-
---}} End of automatically maintained section
 
 architecture convert of sign_extend is
 begin	 
