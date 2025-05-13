7c6520a75a447c76c03fbd62d3a3e76ec72f188c
STAR-ONE <144688272+MHMOUD-ELBAZ@users.noreply.github.com>
2025-05-14 00:16:29 +0300

    المرادي بجد

--- Memory/Memory.vhd
+++ Memory/Memory.vhd
@@ -1,4 +1,4 @@
-library IEEE;
+ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
@@ -22,63 +22,63 @@ end entity;
 -- Our Memory is a simple array of 2048 8-bits words [Byte]
 architecture Behavioral of Memory is
     type memArray is array(0 to 2047) of std_logic_vector(7 downto 0); -- Creating custom type
+
+
+ function init_memory return memArray is
+    variable temp_mem : memArray := (others => (others => '0'));
+	begin	   
+		
+    -- Instructions section:
+    -- 0x00000000: lw $t0, 48($zero) => 8C080030
+	temp_mem(0)  := x"8C"; temp_mem(1)  := x"08"; temp_mem(2)  := x"00"; temp_mem(3)  := x"30";
+
+	-- 0x00000004: lw $t1, 52($zero) => 8C090034
+	temp_mem(4)  := x"8C"; temp_mem(5)  := x"09"; temp_mem(6)  := x"00"; temp_mem(7)  := x"34";	
+	
+    -- 0x00000008: add $t2, $t0, $t1 => 01095020
+    temp_mem(8)  := x"01"; temp_mem(9)  := x"09"; temp_mem(10) := x"50"; temp_mem(11) := x"20";	
+	
+	-- 0x0000000C: sw $t2, 56($zero)
+    temp_mem(12) := x"AC"; temp_mem(13) := x"0A"; temp_mem(14) := x"00"; temp_mem(15) := x"38";
     
-    -- Initialize memory with a function to ensure proper simulation behavior
-    function init_memory return memArray is
-        variable temp_mem : memArray := (others => (others => '0'));
-        
-        -- MIPS Instructions for c = a + b:
-        -- 1. lw $t0, 20($zero)   # Load a into $t0
-        -- 2. lw $t1, 24($zero)   # Load b into $t1
-        -- 3. add $t2, $t0, $t1   # $t2 = $t0 + $t1 (a + b)
-        -- 4. sw $t2, 28($zero)   # Store result in c
-    begin
-        -- Instruction 1: lw $t0, 20($zero) = x"8C080014"
-        temp_mem(0) := x"8C";  -- Most significant byte
-        temp_mem(1) := x"08";
-        temp_mem(2) := x"00";
-        temp_mem(3) := x"14";  -- Least significant byte
-        
-        -- Instruction 2: lw $t1, 24($zero) = x"8C090018"
-        temp_mem(4) := x"8C";
-        temp_mem(5) := x"09";
-        temp_mem(6) := x"00";
-        temp_mem(7) := x"18";
-        
-        -- Instruction 3: add $t2, $t0, $t1 = x"01095020"
-        temp_mem(8) := x"01";
-        temp_mem(9) := x"09";
-        temp_mem(10) := x"50";
-        temp_mem(11) := x"20";
-        
-        -- Instruction 4: sw $t2, 28($zero) = x"AC0A001C"
-        temp_mem(12) := x"AC";
-        temp_mem(13) := x"0A";
-        temp_mem(14) := x"00";
-        temp_mem(15) := x"1C";
-        
-        -- Data value for variable a = 10 (0x0000000A)
-        temp_mem(20) := x"00";
-        temp_mem(21) := x"00";
-        temp_mem(22) := x"00";
-        temp_mem(23) := x"0A";
-        
-        -- Data value for variable b = 15 (0x0000000F)
-        temp_mem(24) := x"00";
-        temp_mem(25) := x"00";
-        temp_mem(26) := x"00";
-        temp_mem(27) := x"0F";
-        
-        -- Location for variable c (will be filled with 25 (0x00000019) after execution)
-        temp_mem(28) := x"00";
-        temp_mem(29) := x"00";
-        temp_mem(30) := x"00";
-        temp_mem(31) := x"00";
-        
-        return temp_mem;
-    end function;
-    
-    -- Use the initialization function
+	-- 0x00000010: sub $t3, $t0, $t1 => 01095822
+    temp_mem(16) := x"01"; temp_mem(17) := x"09"; temp_mem(18) := x"58"; temp_mem(19) := x"22";
+
+    -- 0x00000014: and $t4, $t0, $t1 => 01096124
+    temp_mem(20) := x"01"; temp_mem(21) := x"09"; temp_mem(22) := x"61"; temp_mem(23) := x"24";
+
+    -- 0x00000018: nor $t5, $t0, $t1 => 01096927
+    temp_mem(24) := x"01"; temp_mem(25) := x"09"; temp_mem(26) := x"69"; temp_mem(27) := x"27";
+
+    -- 0x0000001C: sll $t6, $t1, 2 => 00094A00
+    temp_mem(28) := x"00"; temp_mem(29) := x"09"; temp_mem(30) := x"4A"; temp_mem(31) := x"00";
+
+    -- 0x00000020: slt $t7, $t0, $t1 => 0109712A
+    temp_mem(32) := x"01"; temp_mem(33) := x"09"; temp_mem(34) := x"71"; temp_mem(35) := x"2A";
+
+    -- 0x00000024: beq $t0, $t1, 1 => 11090001
+    temp_mem(36) := x"11"; temp_mem(37) := x"09"; temp_mem(38) := x"00"; temp_mem(39) := x"01";
+
+    -- 0x00000028: addi $s0, $zero, 50 => 20100032
+    temp_mem(40) := x"20"; temp_mem(41) := x"10"; temp_mem(42) := x"00"; temp_mem(43) := x"32";
+
+    -- 0x0000002C: j 0x00000020 => 08000008
+	temp_mem(44) := x"08"; temp_mem(45) := x"00"; temp_mem(46) := x"00"; temp_mem(47) := x"08";	 
+	
+    -- Data section:
+    -- 0x00000030: variable a = 10 (0x0000000A)
+    temp_mem(48) := x"00"; temp_mem(49) := x"00"; temp_mem(50) := x"00"; temp_mem(51) := x"0A";
+
+    -- 0x00000034: variable b = 15 (0x0000000F)
+    temp_mem(52) := x"00"; temp_mem(53) := x"00"; temp_mem(54) := x"00"; temp_mem(55) := x"0F";
+
+    -- 0x00000038: variable c (output) = 0 (placeholder)
+    temp_mem(56) := x"00"; temp_mem(57) := x"00"; temp_mem(58) := x"00"; temp_mem(59) := x"00";
+
+    return temp_mem;
+end function;
+
+
     signal Memory: memArray := init_memory;
     
 begin
